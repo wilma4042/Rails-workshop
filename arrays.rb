da8cbd53414130b83495e507772c037493302da0
Wilma Odalo <odalowilma@gmail.com>
2017-09-09 17:16:58 +0300

    add arrays

---
+++ arrays.rb
@@ -0,0 +1,16 @@
+rgnb = ["rachel","natasha","stella"]
+rgnb<< "cecilia" #appanding to an array
+p rgnb
+#arrays begin at 0
+rgnb[1] ="wilmer"
+p rgnb
+#other array method
+
+rgkla = ["keziah", "claire"]
+
+p rgnb + rgkla
+p rgnb.first
+p rgnb.last
+p rgnb.length
+p rgnb.index("stella")
+p rgnb.sort
---
+++ numbers.rb
@@ -0,0 +1,9 @@
+p 5
+p 5 + 7
+p 8* 2
+-6
+ 1_000_000 #showing large numbers
+#true division
+6.0/5
+#integer division
+6/5
