2d3b12a531a978cbfdaea5070d71dac042d6d0a3
Wilma Odalo <odalowilma@gmail.com>
2017-09-09 16:46:28 +0300

    hello

---
+++ hello.rb
@@ -0,0 +1,21 @@
+puts "hello world"
+#print hello world
+
+=begin 
+ multiline comment
+ this code
+ another comment
+=end
+#string -use of " " to print to the user
+puts 'as single quotes'
+puts "double"
+#double used for interpolation but not single
+age = 20
+puts "my age is #{age}"
+ puts 'my age is #{age}'
+#string methods
+puts "firstname" + "second name" #concatenation/combining
+puts "hi" *3
+puts "string".upcase
+puts "mystring".length
+puts'mystring.reverse
